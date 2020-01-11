8139b5b18933e94113bd7c134586987dd9301307
mkraetke <mkraetke@gmx.de>
2020-01-11 15:40:57 +0100

    info class

---
+++ src/main/java/io/transpect/basex/extensions/subversion/XSvnInfo.java
@@ -0,0 +1,68 @@
+package io.transpect.basex.extensions.subversion;
+
+import java.util.HashMap;
+
+import java.io.File;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.wc.SVNWCClient;
+import org.tmatesoft.svn.core.wc.SVNRevision;
+import org.tmatesoft.svn.core.wc.SVNInfo;
+
+import io.transpect.basex.extensions.subversion.XSvnConnect;
+import io.transpect.basex.extensions.subversion.XSvnXmlReport;
+/**
+ * This class provides the svn info command as 
+ * XML Calabash extension step for XProc. The class 
+ * connects to a Subversion repository and provides 
+ * the results as XML document.
+ *
+ * @see XSvnConnect
+ */
+public class XSvnInfo {
+
+    public String XSvnInfo (String url, String username, String password) {
+        XSvnXmlReport report = new XSvnXmlReport();
+	try{
+	    XSvnConnect connection = new XSvnConnect(url, username, password);
+	    SVNWCClient client = connection.getClientManager().getWCClient();
+            SVNInfo info;
+            if(connection.isRemote()){
+                info = client.doInfo(connection.getSVNURL(), SVNRevision.HEAD, SVNRevision.HEAD);
+            } else {
+                info = client.doInfo(new File(url), SVNRevision.HEAD);
+            }
+            HashMap<String, String> results = getSVNInfo(info);
+	    try {
+		String xmlResult = report.createXmlResult(results);
+		return xmlResult;
+	    } catch (XMLStreamException xse) {
+		return xse.getMessage();
+	    }
+	}catch(SVNException svne){
+	    try {
+		String xmlError = report.createXmlError(svne.getMessage());
+		return xmlError;
+	    } catch (XMLStreamException xse) {
+		return xse.getMessage();
+	    }
+	}
+    }
+    /**
+     * Create a HashMap from a SVNInfo object
+     */
+    private HashMap<String, String> getSVNInfo(SVNInfo info){
+        HashMap<String, String> results = new HashMap<String, String>();
+	results.put("url", info.getURL().toString());
+	results.put("author", info.getAuthor());
+	results.put("date", info.getCommittedDate().toString());
+	results.put("uuid", info.getRepositoryUUID());
+	results.put("rev", String.valueOf(info.getCommittedRevision().getNumber()));
+	results.put("path", info.getPath());
+	results.put("root-url", info.getRepositoryRootURL().toString());
+	results.put("nodekind", info.getKind().toString());
+	return results;
+    }
+}
